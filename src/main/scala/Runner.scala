c69ee139e2c2cb79dd8c7b535aa8b12c5d8b8671
QAC-DFiske <danny.fiske@qa.com>
2018-09-10 12:52:57 +0100

    WIP Add streaming section.

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,12 +1,14 @@
+import BatchRunner._
 import batch.SimpleEstimationBatch.SimpleEstimationBatch
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs
 import org.apache.hadoop.fs.{FileSystem, FileUtil}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 
 import scala.util.Try
 
-object Runner {
+object Config {
 
   // Create Spark session.
   val spark: SparkSession = SparkSession.builder().master("local[*]").appName("lambda").getOrCreate()
@@ -16,45 +18,44 @@ object Runner {
   val TIME_COLUMN = "time"
   val IDENTIFIER_COLUMN = "reporting_unit"
   val TARGET_COLUMN = "turnover"
-  val EXTERNAL_PATH = reflect.io.Path("./resources/external")
-  val INGESTION_PATH = reflect.io.Path(".resources/ingestion")
-  val BATCH_RAW_PATH = reflect.io.Path("./resources/storage/raw")
-  val BATCH_PROCESSED_PATH = reflect.io.Path("./resources/storage/processed")
 
-  /**
-    * Coalesce all PART files into single file and rename.
+  // Set IO method constants.
+  val INGESTION_PATH = reflect.io.Path(".resources/ingestion")
+  val RAW_PATH = reflect.io.Path("./resources/storage/raw")
+  val PROCESSED_PATH = reflect.io.Path("./resources/storage/processed")
+  val HADOOP_CONFIG = new Configuration()
+  val HDFS: FileSystem = FileSystem.get(HADOOP_CONFIG)
+}
+
+object BatchRunner {
+
+  import Config._
+
+  /** Coalesce all PART files into single file and rename.
     *
     * @param srcPath String - Directory containing PART files.
     * @param dstPath String - Path to save new file (implicitly set name here)
     */
   def merge(srcPath: String, dstPath: String): Unit =  {
-
-    val hadoopConfig = new Configuration()
-    val hdfs = FileSystem.get(hadoopConfig)
-    FileUtil.copyMerge(hdfs, new fs.Path(srcPath), hdfs, new fs.Path(dstPath), false, hadoopConfig, null)
+    FileUtil.copyMerge(HDFS, new fs.Path(srcPath), HDFS, new fs.Path(dstPath), false, HADOOP_CONFIG, null)
   }
 
-  /**
-    * Copy file to new directory.
+  /** Copy file to new directory.
     *
     * @param srcPath String - Path to file for copying.
     * @param dstPath String - Destination path.
     */
   def copy(srcPath: String, dstPath: String): Unit =  {
-
-    val hadoopConfig = new Configuration()
-    val hdfs = FileSystem.get(hadoopConfig)
-    FileUtil.copy(new java.io.File(srcPath), hdfs, new fs.Path(dstPath), false, hadoopConfig)
+    FileUtil.copy(new java.io.File(srcPath), HDFS, new fs.Path(dstPath), false, HADOOP_CONFIG)
   }
 
   def main(args: Array[String]): Unit = {
 
     // Delete created directory if exists.
-    Try(BATCH_PROCESSED_PATH.deleteRecursively)
-    Try(INGESTION_PATH.deleteRecursively)
+    Try(PROCESSED_PATH.deleteRecursively)
 
     // Read batch data into DataFrame.
-    val batchInput = spark.read.json(BATCH_RAW_PATH + "/batchRaw.json")
+    val batchInput = spark.read.json(RAW_PATH + "/batchRaw.json")
     println("Batch input:")
     batchInput.show()
 
@@ -64,11 +65,38 @@ object Runner {
     batchProcessed.show()
 
     // Write to temporary directory, coalesce part files into permanent directory, delete temporary directory.
-    batchProcessed.write.json(BATCH_PROCESSED_PATH + "/batchProcessedTemp.json")
-    merge(BATCH_PROCESSED_PATH + "/batchProcessedTemp.json", BATCH_PROCESSED_PATH.path + "/batchProcessed.json")
-    Try(reflect.io.Path(BATCH_PROCESSED_PATH + "/batchProcessedTemp.json").deleteRecursively)
+    batchProcessed.write.json(PROCESSED_PATH + "/batchProcessedTemp.json")
+    merge(PROCESSED_PATH + "/batchProcessedTemp.json", PROCESSED_PATH.path + "/batchProcessed.json")
+    Try(reflect.io.Path(PROCESSED_PATH + "/batchProcessedTemp.json").deleteRecursively)
+  }
+}
 
-    // Simulate new data arriving into the system.
-    copy(EXTERNAL_PATH + "/streamIngest.json", INGESTION_PATH + "/streamIngest.json")
+object StreamRunner {
+
+  import Config._
+
+  def main(args: Array[String]): Unit = {
+
+    // Read processed data for join later.
+    val streamJoin = spark.read.json(PROCESSED_PATH + "/batchProcessed.json").select(TIME_COLUMN, "weight")
+
+    // Define schema.
+    val streamSchema = StructType(Seq(StructField("time", StringType, false),
+                                      StructField("reporting_unit", StringType, false),
+                                      StructField("turnover", DoubleType, true)))
+
+    val simpleEstimateInputStream = spark.readStream.schema(streamSchema).json("./resources/ingestion")
+
+    val simpleEstimateTransformStream = simpleEstimateInputStream.join(streamJoin, List(TIME_COLUMN))
+
+    val simpleEstimateWriteStream = simpleEstimateTransformStream.writeStream
+      .format("json")
+      .option("path", "./resources/storage/processed")
+      .option("checkpointLocation", "checkpoint")
+      .start()
+
+    simpleEstimateWriteStream.awaitTermination()
+
+    // TODO: get most recent weight.
   }
 }
